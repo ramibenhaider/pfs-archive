ea09e103c29d897b225dfb21f76fb33644689454
ramibenhaider <ramibenhaider@gmail.com>
2026-04-18 22:35:51 +0300

    Fix: Storage in controller

--- app/Http/Controllers/User/DocumentController.php
+++ app/Http/Controllers/User/DocumentController.php
@@ -10,7 +10,7 @@ use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\URL;
-use Illuminate\support\Facades\Storage;
+use Illuminate\Support\Facades\Storage;
 
 class DocumentController extends Controller
 {
