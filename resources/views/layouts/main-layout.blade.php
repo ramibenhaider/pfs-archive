1284b4129022ea7bd904e5c249ac321af6b48954
ramibenhaider <ramibenhaider@gmail.com>
2026-05-12 00:18:25 +0300

    Fix

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -304,7 +304,6 @@
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
-    <script src="{{ asset('script.js') }}"></script>
     @stack('scripts')
 
 </body>
